4b4285500beee2dc6266e797da1128a55756e090
Luca Borsato <borsato.luca@gmail.com>
2018-04-26 16:21:17 +0200

    Modified transitmodel.py and changed tabs to space (my choice).

--- batman/plots.py
+++ batman/plots.py
@@ -32,56 +32,56 @@ def wrapper(func, *args, **kwargs):
 
 
 def make_plots():
-	import matplotlib.pyplot as plt
-	"""zs = np.linspace(0., 1., 1000)
-	rp = 0.1
-	wrapped = wrapper(_quadratic_ld._quadratic_ld, zs, rp, 0.1, 0.3, 1)
-	t = timeit.timeit(wrapped,number=10000)
-	print("time:", t)"""
-	"""zs = np.linspace(0., 1., 1000)
-	rp = 0.1
-	u = [0., 0.7, 0.0, -0.3]
-	f = _nonlinear_ld._nonlinear_ld(zs, rp, u[0], u[1], u[2], u[3], 1.0e-2, 4)
-	fhi = _nonlinear_ld._nonlinear_ld(zs, rp, u[0], u[1], u[2], u[3], 1.0e-4, 4)
-	fquad = occultquad.occultquad(zs, rp, 0.1, 0.3, 4)
-	#for i in range(len(f)): print "z, fnl, fquad", zs[i], f[i], fquad[i]
+  import matplotlib.pyplot as plt
+  """zs = np.linspace(0., 1., 1000)
+  rp = 0.1
+  wrapped = wrapper(_quadratic_ld._quadratic_ld, zs, rp, 0.1, 0.3, 1)
+  t = timeit.timeit(wrapped,number=10000)
+  print("time:", t)"""
+  """zs = np.linspace(0., 1., 1000)
+  rp = 0.1
+  u = [0., 0.7, 0.0, -0.3]
+  f = _nonlinear_ld._nonlinear_ld(zs, rp, u[0], u[1], u[2], u[3], 1.0e-2, 4)
+  fhi = _nonlinear_ld._nonlinear_ld(zs, rp, u[0], u[1], u[2], u[3], 1.0e-4, 4)
+  fquad = occultquad.occultquad(zs, rp, 0.1, 0.3, 4)
+  #for i in range(len(f)): print "z, fnl, fquad", zs[i], f[i], fquad[i]
 
-	for i in range(1,16):
-		wrapped = wrapper(occultquad.occultquad, zs, rp, 0.1, 0.3, i)
-		t = timeit.timeit(wrapped,number=1)
-		print i, t
+  for i in range(1,16):
+    wrapped = wrapper(occultquad.occultquad, zs, rp, 0.1, 0.3, i)
+    t = timeit.timeit(wrapped,number=1)
+    print i, t
 
-	plt.plot(zs, (f - fhi)*1.0e6)
-	plt.plot(zs, (fhi - fquad)*1.0e6, color='r')
-	plt.axvline(0.9)
-	plt.show()"""
+  plt.plot(zs, (f - fhi)*1.0e6)
+  plt.plot(zs, (fhi - fquad)*1.0e6, color='r')
+  plt.axvline(0.9)
+  plt.show()"""
 
 
-	#generates Figure FIXME: max err as a function of function call time
-	zs = np.linspace(0., 1., 1000)
-	rp = 0.1
-	u = [0., 0.7, 0.0, -0.3]
-	n = 20
-	ts = []
-	errs = []
-	f_ref = _nonlinear_ld._nonlinear_ld(zs, rp, u[0], u[1], u[2], u[3], 1.0e-4, 4)
-	fac = np.logspace(-3, -1, n)
-	for i in range(n):
-		wrapped = wrapper(_nonlinear_ld._nonlinear_ld, zs, rp, u[0], u[1], u[2], u[3], fac[i], 1)
-		t = timeit.timeit(wrapped,number=10)/10.
-		ts.append(t)
-		print(t)
-		f= _nonlinear_ld._nonlinear_ld(zs, rp, u[0], u[1], u[2], u[3], fac[i], 12)
-		err = np.max(np.abs(f - f_ref))
-		errs.append(err)
-	plt.plot(np.array(ts), np.array(errs)*1.0e6, color='k')
-	plt.xlim((1.0e-3, 1.0e-1))
-	plt.yscale('log')
-	plt.xscale('log')
-	plt.xlabel("Time (s)")
-	plt.ylabel("Max Err (ppm)")
-	plt.show()
-	
+  #generates Figure FIXME: max err as a function of function call time
+  zs = np.linspace(0., 1., 1000)
+  rp = 0.1
+  u = [0., 0.7, 0.0, -0.3]
+  n = 20
+  ts = []
+  errs = []
+  f_ref = _nonlinear_ld._nonlinear_ld(zs, rp, u[0], u[1], u[2], u[3], 1.0e-4, 4)
+  fac = np.logspace(-3, -1, n)
+  for i in range(n):
+    wrapped = wrapper(_nonlinear_ld._nonlinear_ld, zs, rp, u[0], u[1], u[2], u[3], fac[i], 1)
+    t = timeit.timeit(wrapped,number=10)/10.
+    ts.append(t)
+    print(t)
+    f= _nonlinear_ld._nonlinear_ld(zs, rp, u[0], u[1], u[2], u[3], fac[i], 12)
+    err = np.max(np.abs(f - f_ref))
+    errs.append(err)
+  plt.plot(np.array(ts), np.array(errs)*1.0e6, color='k')
+  plt.xlim((1.0e-3, 1.0e-1))
+  plt.yscale('log')
+  plt.xscale('log')
+  plt.xlabel("Time (s)")
+  plt.ylabel("Max Err (ppm)")
+  plt.show()
+  
 
-	
+  
 
--- batman/tests.py
+++ batman/tests.py
@@ -29,85 +29,85 @@ def wrapper(func, *args, **kwargs):
     return wrapped
 
 def test():
-	print("\nStarting tests...\n")
-	failures = 0
+  print("\nStarting tests...\n")
+  failures = 0
 
-	params = TransitParams()
-	params.t0 = 0.
-	params.per = 1.0
-	params.rp = 0.1
-	params.a = 15.0
-	params.inc = 90.
-	params.ecc = 0.
-	params.w = 90. 
-	t = np.linspace(-0.01, 0.01, 1000)
+  params = TransitParams()
+  params.t0 = 0.
+  params.per = 1.0
+  params.rp = 0.1
+  params.a = 15.0
+  params.inc = 90.
+  params.ecc = 0.
+  params.w = 90. 
+  t = np.linspace(-0.01, 0.01, 1000)
 
-	u = [[], [0.1], [0.1,0.3], [0.1,-0.3], [0.1,-0.03], [0.0, 0.7, 0.0, -0.3]]
-	ld = ["uniform", "linear", "quadratic", "exponential", "logarithmic", "nonlinear"]
-	
-	#Testing different limb darkening models
-	for i in range(len(u)):
-		params.u = u[i]
-		params.limb_dark = ld[i]
-		print("Testing "+ld[i]+" limb darkening model...\t")
-		m = TransitModel(params, t)
-		lc = m.light_curve(params)
-		if np.min(lc) <= 1.-params.rp**2: print("\ttest passed")
-		else: 
-			print("test failed")
-			failures += 1
+  u = [[], [0.1], [0.1,0.3], [0.1,-0.3], [0.1,-0.03], [0.0, 0.7, 0.0, -0.3]]
+  ld = ["uniform", "linear", "quadratic", "exponential", "logarithmic", "nonlinear"]
+  
+  #Testing different limb darkening models
+  for i in range(len(u)):
+    params.u = u[i]
+    params.limb_dark = ld[i]
+    print("Testing "+ld[i]+" limb darkening model...\t")
+    m = TransitModel(params, t)
+    lc = m.light_curve(params)
+    if np.min(lc) <= 1.-params.rp**2: print("\ttest passed")
+    else: 
+      print("test failed")
+      failures += 1
 
-	#Testing truncation error tolerance
-	params.u = [0.1, 0.3]
-	params.limb_dark = "quadratic"
-	m = TransitModel(params, t)
-	f_ref = m.light_curve(params)
+  #Testing truncation error tolerance
+  params.u = [0.1, 0.3]
+  params.limb_dark = "quadratic"
+  m = TransitModel(params, t)
+  f_ref = m.light_curve(params)
 
-	params.u = np.array([0.0, 0.7, 0.0, -0.3])
-	params.limb_dark = "nonlinear"
+  params.u = np.array([0.0, 0.7, 0.0, -0.3])
+  params.limb_dark = "nonlinear"
 
-	print("\nTesting error tolerance...")
-	max_err = np.logspace(1, -2, 4) 
-	for i in range(len(max_err)):
-		m = TransitModel(params, t, max_err = max_err[i])
-		f = m.light_curve(params)
-		if np.max(1.0e6*(abs(f-f_ref)) < max_err[i]): print("\t"+str(max_err[i])+ " ppm passed")
-		else: 
-			print(str(max_err[i]) + " failed")
-			failures += 1
+  print("\nTesting error tolerance...")
+  max_err = np.logspace(1, -2, 4) 
+  for i in range(len(max_err)):
+    m = TransitModel(params, t, max_err = max_err[i])
+    f = m.light_curve(params)
+    if np.max(1.0e6*(abs(f-f_ref)) < max_err[i]): print("\t"+str(max_err[i])+ " ppm passed")
+    else: 
+      print(str(max_err[i]) + " failed")
+      failures += 1
 
-	print("\nTesting multithreading...")
-	if detect():
-		params.u = [0.1, 0.3]
-		params.limb_dark = "quadratic"
-		m = TransitModel(params, t, nthreads = 1)
-		wrapped = wrapper(m.light_curve, params)
-		t1 = timeit.timeit(wrapped,number=100)
-		m = TransitModel(params, t, nthreads = 2)
-		wrapped = wrapper(m.light_curve, params)
-		t2 = timeit.timeit(wrapped,number=100)
-		if(t1 > 0.9*t2): print("\ttest passed")
-		else:
-			print("\ttest failed")
-			failures +=1
-	else: print("\tOpenMP not supported; test ignored")
+  print("\nTesting multithreading...")
+  if detect():
+    params.u = [0.1, 0.3]
+    params.limb_dark = "quadratic"
+    m = TransitModel(params, t, nthreads = 1)
+    wrapped = wrapper(m.light_curve, params)
+    t1 = timeit.timeit(wrapped,number=100)
+    m = TransitModel(params, t, nthreads = 2)
+    wrapped = wrapper(m.light_curve, params)
+    t2 = timeit.timeit(wrapped,number=100)
+    if(t1 > 0.9*t2): print("\ttest passed")
+    else:
+      print("\ttest failed")
+      failures +=1
+  else: print("\tOpenMP not supported; test ignored")
 
-	if failures == 0: 
-		print("\nCongratulations! all tests passed")
-		print("""
+  if failures == 0: 
+    print("\nCongratulations! all tests passed")
+    print("""
   ____       ____
   )   \     /   (
-   )_  \_V_/  _(
-     )__   __(
+  )_  \_V_/  _(
+    )__   __(
         `-'""")
-	else: 
-		print("Uh oh; tests finished with " + "{0}".format(failures) + " failures")
-	
-		print("""
-         *
+  else: 
+    print("Uh oh; tests finished with " + "{0}".format(failures) + " failures")
+  
+    print("""
+        *
     __  /|
-   /_ \/ |__
+  /_ \/ |__
   *" \.--._ \           
-     ( ^^ )\/
+    ( ^^ )\/
       \__/ *""")
-	         
+          
